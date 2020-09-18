ac888c09059feb665b91b628de41aca61ad3993b
allenwux <allenwu.x@gmail.com>
2020-09-17 20:21:01 -0700

    update

--- tests/mlflow/mlflow_test.py
+++ tests/mlflow/mlflow_test.py
@@ -10,18 +10,3 @@ mlflow.set_tracking_uri("mssql+pyodbc://cloudsa:Yukon900Yukon900@brazilmedia.dat
 
 mlflow.projects.run(uri="https://github.com/allenwux/ml", entry_point="training")
 
-"""
-import pyodbc
-server = 'brazilmedia.database.windows.net'
-database = 'facetsgraph'
-username = 'cloudsa'
-password = 'Yukon900Yukon900'
-driver= '{ODBC Driver 17 for SQL Server}'
-cnxn = pyodbc.connect('DRIVER='+driver+';SERVER='+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+ password)
-cursor = cnxn.cursor()
-cursor.execute("SELECT TOP 20 * FROM facetname")
-row = cursor.fetchone()
-while row:
-    print (str(row[0]))
-    row = cursor.fetchone()
-"""
